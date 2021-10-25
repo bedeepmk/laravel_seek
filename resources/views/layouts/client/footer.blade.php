794a17ca797ce3d1aef253cd373993b6183a2675
SimEunSik <essim92@naver.com>
2021-10-25 14:03:05 +0900

    ...

--- resources/views/layouts/client/footer.blade.php
+++ resources/views/layouts/client/footer.blade.php
@@ -1,5 +1,69 @@
 <footer>
-    <div>
-    <p>This is footer</p>
+    <div class="container-0">
+        <a href="" class="button-adv layout-100">
+            <img src="img/image-adv-0.png" />
+            <span>apple store</span>
+            <span>google play</span>
+        </a>
+    </div>
+    <div class="container-1 layout-100">
+        <div class="icon-0 stylesheet"></div>
+        <a href="5.html" class="guideBtn">Advertiser page</a>
+        <ul class="united-list-parent">
+            <li class="list-parent">
+                <ul class="united-list-child">
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                </ul>
+            </li>
+            <li class="list-parent">
+                <ul class="united-list-child">
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                </ul>
+            </li>
+            <li class="list-parent">
+                <ul class="united-list-child">
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                </ul>
+            </li>
+            <li class="list-parent">
+                <ul class="united-list-child">
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                </ul>
+            </li>
+            <li class="list-parent">
+                <ul class="united-list-child">
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                </ul>
+            </li>
+            <li class="list-parent">
+                <ul class="united-list-child">
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                    <li class="list-child"><a href="">sitemap guide</a></li>
+                </ul>
+            </li>
+        </ul>
     </div>
 </footer>
