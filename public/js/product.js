0918f28a8401bbd2e3bfcb19c5240dd785926405
SimEunSik <essim92@naver.com>
2021-11-12 20:47:32 +0900

    ...

--- public/js/product.js
+++ public/js/product.js
@@ -27,39 +27,41 @@
 //     $('.product-info-box-wrap').removeClass('myswiper')
 // }
 
-let curPos = 0;
-let postion = 0;
-let start_x, end_x;
-const IMAGE_WIDTH = 280;
-const product = document.querySelector(".product-info-box-wrap") 
- 
-product.addEventListener('touchstart', touch_start);
-product.addEventListener('touchend', touch_end);
- 
-function prev(){
-  if(curPos > 0){
-    postion += IMAGE_WIDTH;
-    product.style.transform = `translateX(${postion}px)`;
-    curPos = curPos - 1;
-  }
-}
-function next(){
-  if(curPos < 3){
-    postion -= IMAGE_WIDTH;
-    product.style.transform = `translateX(${postion}px)`;
-    curPos = curPos + 1;
-  }
-}
- 
-function touch_start(event) {
-  start_x = event.touches[0].pageX
-}
- 
-function touch_end(event) {
-  end_x = event.changedTouches[0].pageX;
-  if(start_x > end_x){
-    next();
-  }else{
-    prev();
-  }
-}
+$(function () {
+    let curPos = 0;
+    let postion = 0;
+    let start_x, end_x;
+    const IMAGE_WIDTH = 280;
+    const product = document.querySelector(".product-info-box-wrap") 
+     
+    product.addEventListener('touchstart', touch_start);
+    product.addEventListener('touchend', touch_end);
+     
+    function prev(){
+      if(curPos > 0){
+        postion += IMAGE_WIDTH;
+        product.style.transform = `translateX(${postion}px)`;
+        curPos = curPos - 1;
+      }
+    }
+    function next(){
+      if(curPos < 3){
+        postion -= IMAGE_WIDTH;
+        product.style.transform = `translateX(${postion}px)`;
+        curPos = curPos + 1;
+      }
+    }
+     
+    function touch_start(event) {
+      start_x = event.touches[0].pageX
+    }
+     
+    function touch_end(event) {
+      end_x = event.changedTouches[0].pageX;
+      if(start_x > end_x){
+        next();
+      }else{
+        prev();
+      }
+    }
+  })
